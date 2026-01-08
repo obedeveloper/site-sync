115eeb46a626df0a2fa715eda0984f4d7cb00676
NIYOMUGISHA Obed <obed6@duck.com>
2026-01-08 04:17:24 +0200

    Format and lint

--- src/routes/Nav.svelte
+++ src/routes/Nav.svelte
@@ -1,18 +1,25 @@
 <script lang="ts">
 	import { logIn } from '$lib/auth-client';
+	import { resolve } from '$app/paths';
 	import Logo from './Logo.svelte';
 </script>
 
 <nav class="wrapper my-6 flex items-center justify-between">
-	<a href="/">
+	<a href={resolve('/')}>
 		<Logo class="w-40 sm:w-48"></Logo>
 	</a>
-	<ul class="flex gap-4 sm:text-lg">
+	<ul class="flex gap-4 *:hover:text-text/75 sm:text-lg">
 		<li>
-			<button class="cursor-pointer" onclick={logIn}>Log In</button>
+			<button onclick={logIn}>Log In</button>
 		</li>
 		<li>
-			<a href="/about">About</a>
+			<button>About</button>
 		</li>
 	</ul>
 </nav>
+
+<style>
+	button {
+		cursor: pointer;
+	}
+</style>
