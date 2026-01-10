a1aa5b3e677777a2e469b1191a88589dcb127228
NIYOMUGISHA Obed <obed6@duck.com>
2026-01-11 00:30:52 +0200

    Refactor layout: replace Nav component with header and progress indicator, remove Menu component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
 	import '@fontsource/poppins';
 	import './layout.css';
-	import Nav from './Nav.svelte';
+	import { resolve } from '$app/paths';
+	import Logo from './Logo.svelte';
+	import { navigating } from '$app/state';
+	import { fade } from 'svelte/transition';
 
 	let { children } = $props();
 </script>
@@ -13,6 +16,30 @@
 	<link rel="manifest" href="/site.webmanifest" />
 </svelte:head>
 
-<Nav></Nav>
+<div class="progress fixed top-0 z-10 h-4 w-full">
+	{#if navigating.from}
+		<div transition:fade class="h-full bg-primary"></div>
+	{/if}
+</div>
+
+<header class="wrapper mt-8 mb-6">
+	<a href={resolve('/')}>
+		<Logo class="w-40 sm:w-48"></Logo>
+	</a>
+</header>
 
 {@render children()}
+
+<style>
+	.progress div {
+		width: 0%;
+		margin-inline: auto;
+		animation: progress 1s infinite ease-in-out;
+	}
+
+	@keyframes progress {
+		50% {
+			width: 100%;
+		}
+	}
+</style>
